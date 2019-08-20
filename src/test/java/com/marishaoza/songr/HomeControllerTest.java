9f227e2a7d58889b4d7fd5d35f5189e8a6b280c8
MarishaHoza <marisha.hoza@gmail.com>
2019-08-20 10:26:18 -0700

    main features complete

---
+++ src/test/java/com/marishaoza/songr/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.marishaoza.songr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void reverseText_oneWord() {
+        assertEquals("A single word should not be changed",
+                "hello", HomeController.reverseText("hello"));
+    }
+
+    @Test
+    public void reverseText_manyWords() {
+        assertEquals("Words should be returned in reverse order",
+                "reversed be should this", HomeController.reverseText("this should be reversed"));
+    }
+
+    @Test
+    public void reverseText_emptyString() {
+        assertEquals("An empty string should remain empty",
+                "", HomeController.reverseText(""));
+    }
+
+    @Test ( expected = NullPointerException.class )
+    public void reverseText_null() {
+        // an error should be thrown if reverseText is called on null
+        HomeController.reverseText(null);
+    }
+}
